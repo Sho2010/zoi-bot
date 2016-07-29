f12867ea6c26f56dfae1e80f4b5765b989ff8d0b
sho naito <sho20100@gmail.com>
2016-07-29 19:24:05 +0900

    定時ぞいは平日のみにする

--- slappy-scripts/zoi.rb
+++ slappy-scripts/zoi.rb
@@ -17,11 +17,11 @@ hear 'ぞい' do |event|
   zoi(event)
 end
 
-schedule '5 10 * * *' do
+schedule '5 10 * * 1-5' do
   zoi
 end
 
-schedule '50 18 * * *' do
+schedule '50 18 * * 1-5' do
   say 'そろそろ帰るぞい'
   say 'https://66.media.tumblr.com/a217049d609dbee4d742a10dd1000395/tumblr_ob2kbrOaT21uubj4co1_1280.jpg', channel: '#team-0801'
 end
